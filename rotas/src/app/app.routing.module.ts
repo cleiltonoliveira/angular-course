cc9f453ecae0deef94b2d03f864d79bc0791c77f
cleilton <c.cleiltonoliveira@bol.com.br>
2020-05-30 20:57:55 -0300

    creating routing module

---
+++ rotas/src/app/app.routing.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+
+import { Routes, RouterModule } from '@angular/router';
+
+import { CursosComponent } from './cursos/cursos.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { ModuleWithProviders } from '@angular/core';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
+import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
+
+const appRoutes: Routes = [
+    { path: 'cursos', component: CursosComponent },
+    { path: 'curso/:id', component: CursoDetalheComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'naoEncontrado', component: CursoNaoEncontradoComponent },
+    { path: '', component: HomeComponent }
+];
+
+// export const routing: ModuleWithProviders = RouterModule.forRoot(APP_ROUTES);
+
+@NgModule({
+    imports: [RouterModule.forRoot(appRoutes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {
+
+
+
+
+
+
+}
