de6eb3796d794171ac24fbc150002f85929892b1
cleilton <c.cleiltonoliveira@bol.com.br>
2020-06-06 01:55:18 -0300

    child routes: updating views

--- rotas/src/app/alunos/aluno-detalhe/aluno-detalhe.component.ts
+++ rotas/src/app/alunos/aluno-detalhe/aluno-detalhe.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AlunosService } from '../alunos.service';
 
 @Component({
   selector: 'app-aluno-detalhe',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlunoDetalheComponent implements OnInit {
 
-  constructor() { }
+  aluno: any;
+  inscricao: Subscription;
+
+  constructor(private route: ActivatedRoute, private router: Router, private alunosService: AlunosService) { }
 
   ngOnInit(): void {
+    this.inscricao = this.route.params.subscribe(
+      (params: any) => {
+        let id = params['id'];
+        this.aluno = this.alunosService.getAluno(id);
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.inscricao.unsubscribe();
+  }
+
+  editarContato(){
+    this.router.navigate(['/alunos', this.aluno.id, 'editar'])
   }
 
 }
---
+++ rotas/src/app/alunos/alunos.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlunosService {
+
+  private alunos: any[] = [
+    { id: 1, nome: 'aluno01', email: 'aluno01@email.com' },
+    { id: 2, nome: 'aluno02', email: 'aluno02@email.com' },
+    { id: 3, nome: 'aluno03', email: 'aluno03@email.com' },
+  ];
+
+  constructor() { }
+
+  getAlunos() { return this.alunos; };
+
+  getAluno(id: number) {
+    for (let aluno of this.getAlunos()) {
+      if (aluno.id == id) return aluno;
+    }
+    return null;
+  };
+}
