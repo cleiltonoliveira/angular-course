c401522c44ff544e607901297fb9b5da5a141f54
cleilton <c.cleiltonoliveira@bol.com.br>
2020-06-06 15:37:50 -0300

    configuring lazy loading

--- rotas/src/app/app.module.ts
+++ rotas/src/app/app.module.ts
@@ -1,19 +1,18 @@
 import 'materialize-css';
-import { MaterializeModule } from 'angular2-materialize';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-import { CursosComponent } from './cursos/cursos.component';
+// import { CursosComponent } from './cursos/cursos.component';
 // import { routing } from './app.routing';
-import { CursoDetalheComponent } from './cursos/curso-detalhe/curso-detalhe.component';
-import { CursosService } from './cursos/cursos.service';
-import { CursoNaoEncontradoComponent } from './cursos/curso-nao-encontrado/curso-nao-encontrado.component';
+// import { CursoDetalheComponent } from './cursos/curso-detalhe/curso-detalhe.component';
+// import { CursosService } from './cursos/cursos.service';
+// import { CursoNaoEncontradoComponent } from './cursos/curso-nao-encontrado/curso-nao-encontrado.component';
 import { AppRoutingModule } from './app.routing.module';
-import { CursosModule } from './cursos/cursos.module';
-import { AlunosModule } from './alunos/alunos.module';
+// import { CursosModule } from './cursos/cursos.module';
+// import { AlunosModule } from './alunos/alunos.module';
 
 
 @NgModule({
@@ -26,7 +25,8 @@ import { AlunosModule } from './alunos/alunos.module';
     BrowserModule,
     // MaterializeModule,
     // routing
-    CursosModule, AlunosModule,
+    // CursosModule,
+    // AlunosModule,
     AppRoutingModule
   ],
   bootstrap: [AppComponent]
--- rotas/src/app/cursos/cursos.routing.module.ts
+++ rotas/src/app/cursos/cursos.routing.module.ts
@@ -7,9 +7,9 @@ import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-en
 
 
 const cursosRoutes: Routes = [
-    { path: 'cursos', component: CursosComponent },
-    { path: 'curso/:id', component: CursoDetalheComponent },
-    { path: 'naoEncontrado', component: CursoNaoEncontradoComponent }
+    { path: '', component: CursosComponent },
+    { path: 'naoEncontrado', component: CursoNaoEncontradoComponent },
+    { path: ':id', component: CursoDetalheComponent }
 ];
 
 // export const routing: ModuleWithProviders = RouterModule.forRoot(APP_ROUTES);
