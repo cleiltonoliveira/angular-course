d4f8d4317d318ad7dc14847d74ada4bd159205d8
cleilton <c.cleiltonoliveira@bol.com.br>
2020-06-27 17:45:00 -0300

    add validation to the form

--- forms/src/app/data-form/data-form.component.ts
+++ forms/src/app/data-form/data-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
@@ -10,35 +10,56 @@ import { HttpClient } from '@angular/common/http';
 export class DataFormComponent implements OnInit {
 
   formulario: FormGroup;
-  
+
   constructor(private formBuilder: FormBuilder, private http: HttpClient) { }
 
   ngOnInit(): void {
 
 
     this.formulario = this.formBuilder.group({
-      nome: [null],
-      email: [null]
+      nome: [null, Validators.required],
+      email: [null, [Validators.required, Validators.email]]
     });
 
   }
 
-  onSubmit(){
+  onSubmit() {
 
     console.log(this.formulario.value);
 
     this.http.post('https://httpbin.org/post', JSON.stringify(this.formulario.value))
-    .subscribe(dados => {
-      
-      console.log(dados);
-    
-      // this.formulario.reset();
-      this.resetar();
-    }, 
-    (error : any) => alert("Error"));
+      .subscribe(dados => {
+
+        console.log(dados);
+
+        // this.formulario.reset();
+        this.resetar();
+      },
+        (error: any) => alert("Error"));
   }
 
-  resetar(){
+  resetar() {
     this.formulario.reset();
+  }
+
+  aplicaCssErro(campo) {
+    return {
+      'has-error': this.verificaValidTouched(campo),
+      'has-feedback': this.verificaValidTouched(campo)
+    };
+  }
+
+  verificaValidTouched(campo) {
+    return !this.formulario.get(campo).valid && this.formulario.get(campo).touched;
+  }
+  
+  verificaEmailInvalido() {
+
+    let campoEmail = this.formulario.get('email');
+
+    if(campoEmail.errors){
+
+     return campoEmail.errors['email'] && campoEmail.touched;
+    }
   }
 }
