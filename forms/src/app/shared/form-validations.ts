f5b332ac2fd322e81ffdf225a1c0dec38cc1adb7
cleilton <c.cleiltonoliveira@bol.com.br>
2020-07-03 23:12:37 -0300

    customize the email field validation

--- forms/src/app/shared/form-validations.ts
+++ forms/src/app/shared/form-validations.ts
@@ -1,4 +1,4 @@
-import { FormArray, FormControl } from '@angular/forms';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
 
 export class FormValidations {
 
@@ -34,6 +34,34 @@ export class FormValidations {
             return validacep.test(cep) ? null : { cepInvalido: true };
         }
         return null;
+    }
+
+    static equalsTo(otherField: string) {
+        const validator = (formControl: FormControl) => {
+
+            if (otherField == null) {
+                throw new Error('É necessário informar um campo');
+            }
+            
+            if (!formControl.root || !(<FormGroup>formControl.root).controls) {
+                return null;
+            }
+
+            const field = (<FormGroup>formControl.root).get(otherField);
+
+            if (!field) {
+                throw new Error('É necessário informar um campo válido');
+            }
+
+            if (formControl.value !== field.value) {
+                return { equalsTo: otherField }
+            }
+
+            return null;
+        }
+
+        return validator;
+
     }
 
 }
