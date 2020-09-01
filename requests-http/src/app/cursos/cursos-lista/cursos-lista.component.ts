dfad274a7c163981b68dbaa41c2c788f32dc5ab1
cleilton <c.cleiltonoliveira@bol.com.br>
2020-08-31 23:04:39 -0300

    Catching errors

--- requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
+++ requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CursosService } from '../cursos.service';
 import { Curso } from '../curso';
-import { Observable } from 'rxjs';
+import { Observable, empty, Subject } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Component({
   selector: 'app-cursos-lista',
@@ -13,15 +14,23 @@ export class CursosListaComponent implements OnInit {
 
   // cursos: Curso[];
 
-  cursos$: Observable<Curso[]>
+  cursos$: Observable<Curso[]>;
+  error$ = new Subject<boolean>();
 
   constructor(private service: CursosService) { }
 
   ngOnInit(): void {
     // this.service.list().subscribe(dados => {
     //   this.cursos = dados});
-
-    this.cursos$ = this.service.list();
+    this.onRefresh();
   }
 
+  onRefresh() {
+    this.cursos$ = this.service.list()
+      .pipe(catchError(error => {
+        console.error(error);
+        this.error$.next(true);
+        return empty();
+      }));
+  }
 }
