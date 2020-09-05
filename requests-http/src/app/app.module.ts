0ed906964f02c6a2eeedc39eaabafb13fbc7385d
cleilton <c.cleiltonoliveira@bol.com.br>
2020-09-04 21:37:41 -0300

    Error alert with bootstrap

--- requests-http/src/app/app.module.ts
+++ requests-http/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { CursosModule } from './cursos/cursos.module';
-import { EnviarValorService } from './unsubscribe-rxjs/enviar-valor.service';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,9 @@ import { EnviarValorService } from './unsubscribe-rxjs/enviar-valor.service';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    CursosModule
+    CursosModule,
+    ModalModule.forRoot(),
+    SharedModule
   ],
   bootstrap: [AppComponent]
 })
