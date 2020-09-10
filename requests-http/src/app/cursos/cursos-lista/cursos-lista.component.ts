91a27549ea6ee915148083a8db8cb82f1eee16c8
cleilton <c.cleiltonoliveira@bol.com.br>
2020-09-10 15:20:26 -0300

    Generic confirmation pop-up

--- requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
+++ requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { CursosService } from '../cursos.service';
 import { Curso } from '../curso';
-import { Observable, empty, Subject } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { Observable, empty, Subject, pipe, EMPTY } from 'rxjs';
+import { catchError, take, switchMap } from 'rxjs/operators';
 import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 import { AlertModalComponent } from 'src/app/shared/alert-modal/alert-modal.component';
 import { AlertModalService } from 'src/app/shared/alert-modal.service';
@@ -60,7 +60,21 @@ export class CursosListaComponent implements OnInit {
 
   onDelete(curso) {
     this.cursoSelecionado = curso;
-    this.deleteModalRef = this.modalService.show(this.deleteModal, { class: "modal-sm" })
+    // this.deleteModalRef = this.modalService.show(this.deleteModal, { class: "modal-sm" })
+    const result$ = this.alertService.showConfirm('Confirmação', 'Tem certeza que deseja remover o curso?');
+    result$
+      .pipe(
+        take(1),
+        switchMap(result => result ? this.service.remove(curso.id) : EMPTY)
+      )
+      .subscribe(
+        success => {
+          this.onRefresh();
+        },
+        error => {
+          this.alertService.showAlertDanger('Erro ao remover curso. Tente novamente mais tarde.');
+        }
+      );
   }
 
   onConfirmDelete() {
