d0d96cec0887d907fadb410a4e03637216ae5f95
cleilton <c.cleiltonoliveira@bol.com.br>
2020-09-14 21:20:32 -0300

    Added progress bar

--- requests-http/src/app/upload-file/upload-file/upload-file.component.ts
+++ requests-http/src/app/upload-file/upload-file/upload-file.component.ts
@@ -1,3 +1,4 @@
+import { HttpEvent, HttpEventType } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { UploadFileService } from '../upload-file.service';
@@ -10,6 +11,7 @@ import { UploadFileService } from '../upload-file.service';
 export class UploadFileComponent implements OnInit {
 
   files: Set<File>;
+  progress = 0;
 
   constructor(private service: UploadFileService) { }
 
@@ -29,12 +31,23 @@ export class UploadFileComponent implements OnInit {
     }
 
     document.getElementById('customFileLabel').innerHTML = fileNames.join(', ');
+    this.progress = 0;
   }
 
   onUpload() {
     if (this.files && this.files.size > 0) {
       this.service.upload(this.files, environment.BASE_URL + '/upload').subscribe(
-        response => console.log(response)
+        (event: HttpEvent<Object>) => {
+          console.log(event);
+
+          if (event.type === HttpEventType.Response) {
+            console.log('Upload concluído');
+          } else if (event.type === HttpEventType.UploadProgress) {
+            const percentDone = Math.round((event.loaded * 100) / event.total);
+            console.log('Progresso', percentDone);
+            this.progress = percentDone;
+          }
+        }
       );
     }
   }
