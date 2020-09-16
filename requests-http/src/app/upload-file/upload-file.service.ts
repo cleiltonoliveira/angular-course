c5cd43de74606fe15f5e3a76fcd31892504cb266
cleilton <c.cleiltonoliveira@bol.com.br>
2020-09-16 10:27:42 -0300

    File download

--- requests-http/src/app/upload-file/upload-file.service.ts
+++ requests-http/src/app/upload-file/upload-file.service.ts
@@ -21,4 +21,40 @@ export class UploadFileService {
       reportProgress: true
     } );
   }
+
+  download(url: string){
+    return this.http.get(url, {
+      responseType: 'blob' as 'json'
+    });
+  }
+
+  handleFile(res, fileName: string){
+
+    const file = new Blob([res], {
+      type: res.type
+    });
+
+    //IE
+    if (window.navigator && window.navigator.msSaveOrOpenBlob) {
+      window.navigator.msSaveOrOpenBlob(file);
+      return;
+    }
+
+    const blob = window.URL.createObjectURL(file);
+    const link = document.createElement('a');
+
+    link.href = blob;
+    link.download = fileName;
+  
+    link.dispatchEvent(new MouseEvent('click', { // Firefox
+      bubbles: true,
+      cancelable: true,
+      view: window
+    }));
+
+    setTimeout(() => {
+      window.URL.revokeObjectURL(blob);
+      link.remove();
+    }, 100);
+  }
 }
